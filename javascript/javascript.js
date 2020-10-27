be77d79dddd335b6d8c8c7c78dbb330d8b1da2a0
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-10-27 15:31:29 -0300

    alterações

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,11 +5,14 @@ var checkbox = document.querySelector('#esconder');
 checkbox.addEventListener('click', function () {
 
     var menu = document.querySelector('#lateral');
-
+    var menu_menor = document.querySelector('#lateral_menor');
+    
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
+        menu_menor.style.display = 'none';
     } else {
         menu.style.display = 'none';
+        menu_menor.style.display = 'flex'
     }
 })
 
