a87e9578d30d5747b145b073b5a5f98f86fc28be
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-11-04 17:43:18 -0300

    alterações
    Redimensionando os cards ao clicar para o menu lateral ficar menor.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,21 @@
-/* Fazendo o menu lateral se esconder */
 var checkbox = document.querySelector('#esconder');
 
-/* Escondendo o menu lateral maior e aparecendo o menor */
 checkbox.addEventListener('click', function () {
 
-    var menu = document.querySelector('#lateral');
-    var menu_menor = document.querySelector('#lateral_menor');
-    
+    var menu = document.getElementById("lateral");
+    var menu_menor = document.getElementById("lateral_menor");
+    var conteudo = document.getElementById("conteudoVideo");
+    var video = document.getElementById("video");
+
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
         menu_menor.style.display = 'none';
+        conteudo.className = 'conteudo-video';
+        video.className = 'video';
     } else {
         menu.style.display = 'none';
-        menu_menor.style.display = 'flex'
+        menu_menor.style.display = 'flex';
+        conteudo.className = 'conteudo-video-max';
+        video.className = 'video-max';
     }
 })
