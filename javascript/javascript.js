c7f9eab6386bfb1ef8b9c9da82723684bfcdceee
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-11-09 08:30:45 -0300

    alterações

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,20 +2,24 @@ var checkbox = document.querySelector('#esconder');
 
 checkbox.addEventListener('click', function () {
 
-    var menu = document.getElementById("lateral");
-    var menu_menor = document.getElementById("lateral_menor");
-    var conteudo = document.getElementById("conteudoVideo");
-    var video = document.getElementById("video");
+    let menu = document.getElementById("lateral");
+    let menu_menor = document.getElementById("lateral_menor");
+    let conteudo = document.getElementById("conteudoVideo");
+    let video = document.getElementById("video");
 
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
         menu_menor.style.display = 'none';
         conteudo.className = 'conteudo-video';
-        video.className = 'video';
+        while (video.className.length = 'video-max') {
+            video.className = 'video';
+          }
     } else {
         menu.style.display = 'none';
         menu_menor.style.display = 'flex';
         conteudo.className = 'conteudo-video-max';
-        video.className = 'video-max';
+        while (video.className.length = 'video') {
+            video.className = 'video-max';
+          }
     }
 })
