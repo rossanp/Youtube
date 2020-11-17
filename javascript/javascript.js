eacee5b5961fe51d93ef9f7d0cf425d0c37459d0
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-11-17 16:02:37 -0300

    alterações

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,22 +6,42 @@ checkbox.addEventListener('click', function () {
     let menu_menor = document.getElementById("lateral_menor");
     let conteudo = document.getElementById("conteudoVideo");
     let video = document.getElementById("video");
+    let iframe = document.getElementById("iframe");
 
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
         menu_menor.style.display = 'none';
         conteudo.className = 'conteudo-video';
-        /* video.className = 'video'; */
+        video.style.marginRight = '15px';
+        iframe.style.height= '200px';
+        iframe.style.width= '360px';
     } else {
         menu.style.display = 'none';
         menu_menor.style.display = 'flex';
         conteudo.className = 'conteudo-video-max';
-        /* video.className = 'video-max'; */
-    }
+        video.style.marginRight = '0px';
+        iframe.style.height= '190px';
+        iframe.style.width= '345px';
 
-/*     if (video.className === 'video') {
-        for (let i = 0; i < video.className.length; i++) {
-            video.className = 'video-max';
-        }
-    } */
+    }
 })
+
+/* .video {
+    margin-bottom: 75px;
+    margin-right: 15px;
+}
+
+.video-max {
+    margin-bottom: 75px;
+    margin-right: 0px;
+} 
+
+#iframe {
+    height: 200px;
+    width: 360px;
+}
+
+#video-max iframe {
+    height: 190px;
+    width: 345px;
+}*/
