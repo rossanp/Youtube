d9a89c6b247aaa9bd5de96395c3d4420a4f6b0b0
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-01-13 18:25:36 -0300

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,23 +6,26 @@ checkbox.addEventListener('click', function () {
     let menu_menor = document.getElementById("lateral_menor");
     let conteudo = document.getElementById("conteudoVideo");
     let video = document.getElementById("video");
-    let iframe = document.getElementById("iframe");
+    var iframe = document.querySelectorAll("iframe");
 
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
         menu_menor.style.display = 'none';
         conteudo.className = 'conteudo-video';
         video.style.marginRight = '15px';
-        iframe.style.height= '200px';
-        iframe.style.width= '360px';
+        for(x = 0; x < iframe.length; x++){
+            iframe[x].style.height= '200px';
+            iframe[x].style.width= '360px';
+        }
     } else {
         menu.style.display = 'none';
         menu_menor.style.display = 'flex';
         conteudo.className = 'conteudo-video-max';
         video.style.marginRight = '0px';
-        iframe.style.height= '190px';
-        iframe.style.width= '345px';
-
+        for(x = 0; x < iframe.length; x++){
+        iframe[x].style.height= '190px';
+        iframe[x].style.width= '345px';
+        }
     }
 })
 
