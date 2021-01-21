8ce117e5a2921fc8f0b10d2e2de2be6b79a8b07f
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-01-21 13:12:59 -0300

    inclusão de imgs, mudanças no main e js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,50 +1,41 @@
+/* Buscando o estado do checkbox */
 var checkbox = document.querySelector('#esconder');
 
+/* Criando a função ao ouvir o checkbox */
 checkbox.addEventListener('click', function () {
 
     let menu = document.getElementById("lateral");
     let menu_menor = document.getElementById("lateral_menor");
     let conteudo = document.getElementById("conteudoVideo");
     let video = document.getElementById("video");
-    var iframe = document.querySelectorAll("iframe");
+    let iframe = document.getElementById("iframe");
 
+    /* Modificando o CSS */
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
         menu_menor.style.display = 'none';
         conteudo.className = 'conteudo-video';
         video.style.marginRight = '15px';
-        for(x = 0; x < iframe.length; x++){
-            iframe[x].style.height= '200px';
+        /* video.remove.style.width = "min-content"; */
+        iframe.style.width= '345px';
+        /* for(x = 0; x < iframe.length; x++){
+            iframe[x].style.height= '190px';
             iframe[x].style.width= '360px';
-        }
+        } */
     } else {
         menu.style.display = 'none';
         menu_menor.style.display = 'flex';
         conteudo.className = 'conteudo-video-max';
-        video.style.marginRight = '0px';
-        for(x = 0; x < iframe.length; x++){
-        iframe[x].style.height= '190px';
-        iframe[x].style.width= '345px';
-        }
+        video.style.marginRight = '10px';
+        video.add.style.width = "min-content";
+        iframe.style.width= '345px';
+        /* for(x = 0; x < iframe.length; x++){
+            iframe[x].style.height= '190px';
+            iframe[x].style.width= '345px';
+        } */
     }
 })
 
-/* .video {
-    margin-bottom: 75px;
-    margin-right: 15px;
-}
+/*
 
-.video-max {
-    margin-bottom: 75px;
-    margin-right: 0px;
-} 
-
-#iframe {
-    height: 200px;
-    width: 360px;
-}
-
-#video-max iframe {
-    height: 190px;
-    width: 345px;
-}*/
+*/
